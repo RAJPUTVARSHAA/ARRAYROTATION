a1dab3025a64d6933aad5fb24b3619caf8bc6cb5
RAJPUTVARSHAA <165540115+RAJPUTVARSHAA@users.noreply.github.com>
2024-04-09 21:54:07 +0530

    Add files via upload

---
+++ ArrayRotation.java
@@ -0,0 +1,49 @@
+ import java.util.Scanner;
+
+public class ArrayRotation {
+
+    static void reverseArray(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    static void leftRotate(int arr[], int d) {
+        int n = arr.length;
+        d = d % n;
+        reverseArray(arr, 0, d - 1);
+        reverseArray(arr, d, n - 1);
+        reverseArray(arr, 0, n - 1);
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++)
+            System.out.print(arr[i] + " ");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int arr[] = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the number of elements to rotate:");
+        int d = scanner.nextInt();
+
+        leftRotate(arr, d);
+        printArray(arr);
+
+        scanner.close();
+    }
+}
+
